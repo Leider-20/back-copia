c5627c005f90ba645d6b44739ba534540c370ffc
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-10-22 18:14:07 -0500

    feat: Uso de Builder para el mappeo de Usuario

--- src/main/java/co/udea/ssmu/api/model/jpa/dto/UsuarioDTO.java
+++ src/main/java/co/udea/ssmu/api/model/jpa/dto/UsuarioDTO.java
@@ -2,12 +2,14 @@ package co.udea.ssmu.api.model.jpa.dto;
 
 import co.udea.ssmu.api.model.jpa.model.Grupo;
 import co.udea.ssmu.api.model.jpa.model.TipoUsuario;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@Builder
 public class UsuarioDTO {
     private long id;
     private String nombre;
