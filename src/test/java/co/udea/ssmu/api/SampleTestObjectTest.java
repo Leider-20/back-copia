fa35524389262a2f5d1e723d81ef6c2201c36b52
fdejesuscorrea <fdejesus.correa@udea.edu.co>
2023-11-06 18:42:01 -0500

    Create SampleTestObjectTest.java
    Creacion del test para el objeto de prueba de ejemplo para analizar si la cobertura de código aumenta

---
+++ src/test/java/co/udea/ssmu/api/SampleTestObjectTest.java
@@ -0,0 +1,42 @@
+package co.udea.ssmu.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SimpletestObjectTests {
+
+    private SampleTestObject testObject;
+
+    @BeforeEach
+    public void setUp() {
+        testObject = new SimpletestObject();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, testObject.add(2, 3), "2 + 3 should equal 5");
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, testObject.subtract(3, 2), "3 - 2 should equal 1");
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, testObject.multiply(2, 3), "2 * 3 should equal 6");
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, testObject.divide(4, 2), "4 / 2 should equal 2");
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> testObject.divide(1, 0), "Divide by zero should throw IllegalArgumentException");
+    }
+}
